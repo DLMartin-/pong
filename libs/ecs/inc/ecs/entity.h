cae16ca8d8ccd1c8db4d69340382d2f4f3b2cecf
Danny Martin <ashinyacorn@gmail.com>
2020-12-01 21:18:37 -0500

    Added invalid entity ID

--- libs/ecs/inc/ecs/entity.h
+++ libs/ecs/inc/ecs/entity.h
@@ -8,13 +8,19 @@ namespace ecs {
   enum struct entity_version : std::uint8_t {};
 
   constexpr std::uint32_t entity_index_bitmask = 0x00ff'ffff;
-  constexpr std::uint32_t entity_version_bitmask = 0xff00'0000;
+  constexpr std::uint32_t entity_version_bitmask = 0x7f00'0000;
+  constexpr std::uint32_t entity_invalid_bitmask = 0x8000'0000;
+
   inline entity_index get_index(entity_t entity) noexcept {
     return entity_index(static_cast<std::uint32_t>(entity) & entity_index_bitmask);
   } 
 
   inline entity_version get_version(entity_t entity) noexcept {
     return entity_version(static_cast<std::uint32_t>(entity) & entity_version_bitmask);
+  }
+
+  inline bool is_valid(entity_t entity) noexcept {
+    return static_cast<std::uint32_t>(entity) & entity_index_bitmask;
   }
 }
 #endif
