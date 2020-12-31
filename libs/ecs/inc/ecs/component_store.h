06416d740ec0c11e3e087f12d6ec14fcfcee5cf5
Danny Martin <ashinyacorn@gmail.com>
2020-12-30 23:58:51 -0500

    Added ability to check if a component exists on an entity

--- libs/ecs/inc/ecs/component_store.h
+++ libs/ecs/inc/ecs/component_store.h
@@ -3,6 +3,7 @@
 #include "containers/array.h"
 #include "types.h"
 #include <unordered_map>
+#include <unordered_set>
 #include <typeindex>
 
 namespace acorn {
@@ -17,8 +18,18 @@ class component_store {
       if(!component_data_.contains(component_index)) {
         component_data_[component_index] = reinterpret_cast<void*>(new acorn::ecs::containers::array<T>());
       }
+      
+      component_metadata_[component_index].emplace(entity);
 
       (reinterpret_cast<acorn::ecs::containers::array<T>*>(component_data_[component_index]))->get(entity_index) = component;
+    }
+
+    template<typename T>
+    void remove_component(entity_t entity) {
+      auto const component_index = std::type_index(typeid(T));
+      if(component_metadata_.find(component_index) != std::end(component_metadata_)) {
+        component_metadata_[component_index].erase(entity);
+      }
     }
 
     template<typename T>
@@ -31,8 +42,18 @@ class component_store {
 
       return (reinterpret_cast<acorn::ecs::containers::array<T>*>(component_data_[component_index]))->get(entity_index);
     }
+
+    template<typename T>
+    bool has_component(entity_t entity) {
+      auto const component_index = std::type_index(typeid(T));
+      if(component_metadata_.find(component_index) != std::end(component_metadata_)) {
+        return component_metadata_[component_index].contains(entity);
+      } 
+      return false;
+    }
   private:
     std::unordered_map<std::type_index, void*> component_data_{};
+    std::unordered_map<std::type_index, std::unordered_set<entity_t>> component_metadata_{};
 };
 }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -81,14 +81,14 @@ int main(int argc, char** argv) {
   components.insert_component(e1, pos2);
   components.insert_component(e1, sprite);
 
-  std::cout << "position in component store:" << components.get_component<position_t>(e0).x << ", " << components.get_component<position_t>(e0).y << " \n";
-  std::cout << "position in component store:" << components.get_component<position_t>(e1).x << ", " << components.get_component<position_t>(e1).y << " \n";
-
   auto const position_component = components.get_component<position_t>(e0);
   auto const position_component2 = components.get_component<position_t>(e1);
   auto const sprite_component = components.get_component<sprite_t>(e0);
   auto const sprite_component2 = components.get_component<sprite_t>(e1);
+  point_value_t pvc { .value = 0 };
+  int i = 1;
   while(1) {
+    ++i;
     while(SDL_PollEvent(&e)) {
       if(e.type == SDL_KEYDOWN) {
         if(e.key.keysym.sym == SDLK_ESCAPE) {
@@ -103,7 +103,16 @@ int main(int argc, char** argv) {
     
     SDL_Rect pos_rect{.x =static_cast<int>(position_component.x), .y = static_cast<int>(position_component.y), .w = 150, .h = 150};
     SDL_Rect pos_rect2{.x =static_cast<int>(position_component2.x), .y = static_cast<int>(position_component2.y), .w = 50, .h = 50};
-    SDL_RenderCopy(renderer, sprite_component.texture, nullptr, &pos_rect); 
+    if(components.has_component<point_value_t>(e0)) {
+      if(i % 200 == 0) {
+        components.remove_component<point_value_t>(e0);
+      }
+      SDL_RenderCopy(renderer, sprite_component.texture, nullptr, &pos_rect); 
+    } else {
+      if(i % 200 == 0) {
+        components.insert_component(e0, pvc);
+      }
+    }
     SDL_RenderCopy(renderer, sprite_component2.texture, nullptr, &pos_rect2); 
     SDL_RenderPresent(renderer);
   } 
