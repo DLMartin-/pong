efc834a379e508764ad17cb2882f8d50e4863c66
Danny Martin <ashinyacorn@gmail.com>
2021-01-01 02:26:30 -0500

    Added rendering for the bounding box of the ball

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,13 @@ struct velocity_t {
   float y;
 };
 
+struct bounding_box_t {
+  float x;
+  float y;
+  float w;
+  float h;
+};
+
 struct texture_t {
   SDL_Texture* texture;
 };
@@ -65,37 +72,48 @@ int main(int argc, char** argv) {
   velocity_t ball_velocity {.x = .09f, .y = .25f};
   components.insert_component(ball, ball_velocity);
 
+  bounding_box_t ball_bounding_box {.x = 15.f, .y = 15.f, .w = 50.f, .h = 50.f};
+  components.insert_component(ball, ball_bounding_box);
+
   texture_t ball_texture {.texture = tex};
   components.insert_component(ball, ball_texture);
 
   auto const ball_physics_system = [&components, ball]{
     auto& ball_position = components.get_component<position_t>(ball);
+    auto& ball_bounding_box = components.get_component<bounding_box_t>(ball);
     auto const& ball_velocity = components.get_component<velocity_t>(ball);
 
     ball_position.x += ball_velocity.x;
     ball_position.y += ball_velocity.y;
+
+    ball_bounding_box.x = ball_position.x;
+    ball_bounding_box.y = ball_position.y;
   };
 
   auto const ball_check_boundary_collision_system = [&components, ball] {
     auto& ball_position = components.get_component<position_t>(ball);
+    auto& ball_bounding_box = components.get_component<bounding_box_t>(ball);
     auto& ball_velocity = components.get_component<velocity_t>(ball);
 
-    if(ball_position.x < 0) {
+    if(ball_bounding_box.x < 0) {
+      ball_bounding_box.x = 0;
       ball_position.x = 0;
       ball_velocity.x *= -1;
-    } else if (ball_position.x > 700) {
-      ball_position.x = 700;
+    } else if (ball_bounding_box.x + ball_bounding_box.w > 700) {
+      ball_bounding_box.x = 700 - ball_bounding_box.w - 1;
+      ball_position.x = 700 - ball_bounding_box.w - 1;
       ball_velocity.x *= -1;
     }
 
-    if(ball_position.y < 0) {
+    if(ball_bounding_box.y < 0) {
+      ball_bounding_box.y = 0;
       ball_position.y = 0;
       ball_velocity.y *= -1;
-    } else if(ball_position.y > 1440) {
-      ball_position.y = 1440;
+    } else if(ball_bounding_box.y + ball_bounding_box.h > 1440) {
+      ball_bounding_box.y = 1440 - ball_bounding_box.h  - 1;
+      ball_position.y = 1440 - ball_bounding_box.h - 1;
       ball_velocity.y *= -1;
     }
-
   };
 
   auto const render_ball_system = [&components, ball, renderer] {
@@ -104,6 +122,16 @@ int main(int argc, char** argv) {
 
     auto const rect = SDL_Rect{.x = static_cast<int>(ball_position.x), .y = static_cast<int>(ball_position.y), .w = 50, .h = 50};
     SDL_RenderCopy(renderer, ball_texture.texture, nullptr, &rect);
+  };
+
+  auto const render_pong_debug_system = [&components, ball, renderer] {
+    auto const ball_bounding_box = components.get_component<bounding_box_t>(ball);
+
+    auto const rect = SDL_Rect{.x = static_cast<int>(ball_bounding_box.x), .y = static_cast<int>(ball_bounding_box.y), .w = static_cast<int>(ball_bounding_box.w), .h = static_cast<int>(ball_bounding_box.h)};
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    SDL_RenderDrawRect(renderer, &rect);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   };
 
   while(1) {
@@ -123,6 +151,7 @@ int main(int argc, char** argv) {
 
     SDL_RenderClear(renderer);
     render_ball_system(); 
+    render_pong_debug_system();
     SDL_RenderPresent(renderer);
   } 
   return 0;
